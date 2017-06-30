ab5f9605813b2b38ffa40eb859ec62d5c8171488
Adam Silverstein <adam@10up.com>
2017-06-30 09:19:05 -0400

    remove some unused variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 var path         = require('path'),
-	fs           = require( 'fs' ),
 	SOURCE_DIR   = 'src/',
-	BUILD_DIR    = 'build/',
-	autoprefixer = require('autoprefixer'),
 	mediaConfig  = {},
 	mediaBuilds  = ['audiovideo', 'grid', 'models', 'views'];
 
