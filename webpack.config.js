c8af8916ad1a3fc993ed4977045859958b06c169
Adam Silverstein <adam@10up.com>
2017-06-29 23:00:06 -0400

    break up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-var path = require('path'),
-	fs = require( 'fs' ),
-	SOURCE_DIR = 'src/',
-	BUILD_DIR = 'build/',
+var path         = require('path'),
+	fs           = require( 'fs' ),
+	SOURCE_DIR   = 'src/',
+	BUILD_DIR    = 'build/',
 	autoprefixer = require('autoprefixer'),
-	mediaConfig = [],
-	mediaBuilds = ['audiovideo', 'grid', 'models', 'views'];
+	mediaConfig  = {},
+	mediaBuilds  = ['audiovideo', 'grid', 'models', 'views'];
 
 
 mediaBuilds.forEach( function ( build ) {
 	var path = SOURCE_DIR + 'wp-includes/js/media';
-	mediaConfig.push( path + '/' + build + '.manifest.js' );
+	mediaConfig[ build ] = './' + path + '/' + build + '.manifest.js';
 } );
 
 
@@ -17,6 +17,10 @@ console.log( mediaConfig );
 
 module.exports = {
 	entry: mediaConfig,
+	output: {
+		path:     path.join( __dirname, 'build' ),
+		filename: '[name].js'
+	},
 
 	cache: true,
 
