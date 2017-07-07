2c2aded15dab4b06eb9d082c5af6dde9ce14444f
Adam Silverstein <adam@10up.com>
2017-07-07 12:18:50 -0400

    add some additional tests

--- tests/phpunit/tests/formatting/HumanTimeDiff.php
+++ tests/phpunit/tests/formatting/HumanTimeDiff.php
@@ -35,19 +35,34 @@ class Tests_Formatting_HumanTimeDiff extends WP_UnitTestCase {
 				'Test a difference of 2 days.',
 			),
 			array(
+				'2 hours',
+				new DateTime( '2016-01-01 14:29:59' ),
+				'Test a difference of 2 hours, 29 minutes and 59 seconds  - should round down to 2 hours.',
+			),
+			array(
 				'3 hours',
-				new DateTime( '2016-01-01 14:30:01' ),
-				'Test a difference of 2 hours and 30 minutes and 1 second - should round up to 3 hours.',
+				new DateTime( '2016-01-01 14:30:00' ),
+				'Test a difference of 2 hours and 30 minutes - should round up to 3 hours.',
 			),
 			array(
 				'2 months',
-				new DateTime( '2016-02-17 12:00:00' ),
-				'Test a difference of 1 month and 16 days - should round up to 2 months.',
+				new DateTime( '2016-02-15 12:00:00' ),
+				'Test a difference of 1 month and 15 days - should round up to 2 months.',
+			),
+			array(
+				'1 month',
+				new DateTime( '2016-02-14 12:00:00' ),
+				'Test a difference of 1 month and 14 days - should round down to 1 month.',
 			),
 			array(
 				'3 years',
 				new DateTime( '2018-07-02 12:00:00' ),
 				'Test a difference of 2 years 6 months and 1 day, should round up to 3 years.',
+			),
+			array(
+				'2 years',
+				new DateTime( '2018-07-01 12:00:00' ),
+				'Test a difference of 2 years 6 months, should round down to 2 years.',
 			),
 		);
 	}
