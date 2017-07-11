aa3fab14b6a263bf9e73705184941c6336c563d1
Adam Silverstein <adam@10up.com>
2017-07-11 08:44:10 -0400

    move sanitization functions to new file wp-sanitize

--- src/wp-includes/js/utils.js
+++ src/wp-includes/js/utils.js
@@ -196,39 +196,3 @@ function getAllUserSettings() {
 	return wpCookies.getHash( 'wp-settings-' + userSettings.uid ) || {};
 }
 
-window.wp = window.wp || {};
-wp.utils = {
-
-		/**
-		 * Strip HTML tags.
-		 *
-		 * @param string string Text to have the HTML tags striped out of.
-		 * @return Stripped text.
-		 */
-		stripTags: function( string ) {
-			string = string || '';
-
-			return string
-				.replace( /<!--[\s\S]*?(-->|$)/g, '' )
-				.replace( /<(script|style)[^>]*>[\s\S]*?(<\/\1>|$)/ig, '' )
-				.replace( /<\/?[a-z][\s\S]*?(>|$)/ig, '' );
-		},
-
-		/**
-		 * Strip HTML tags and convert HTML entities.
-		 *
-		 * @param text string Text.
-		 * @return Sanitized text.
-		 */
-		sanitizeText: function( text ) {
-			var _text = wp.utils.stripTags( text ),
-				textarea = document.createElement( 'textarea' );
-
-			try {
-				textarea.innerHTML = _text;
-				_text = wp.utils.stripTags( textarea.value );
-			} catch ( er ) {}
-
-			return _text;
-		}
-};
