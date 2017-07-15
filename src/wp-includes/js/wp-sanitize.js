84cdf8f6a79c479c3fd69dc9598d22dfe0b599ea
Adam Silverstein <adam@10up.com>
2017-07-15 11:43:37 -0400

    whitespace cleanup

--- src/wp-includes/js/wp-sanitize.js
+++ src/wp-includes/js/wp-sanitize.js
@@ -9,40 +9,39 @@
 	 */
 	wp.sanitize = {
 
-			/**
-			 * Strip HTML tags.
-			 *
-			 * @param {string} text Text to have the HTML tags striped out of.
-			 *
-			 * @return  Stripped text.
-			 */
-			stripTags: function( text ) {
-				text = text || '';
+		/**
+		 * Strip HTML tags.
+		 *
+		 * @param {string} text Text to have the HTML tags striped out of.
+		 *
+		 * @return  Stripped text.
+		 */
+		stripTags: function( text ) {
+			text = text || '';
 
-				return text
-					.replace( /<!--[\s\S]*?(-->|$)/g, '' )
-					.replace( /<(script|style)[^>]*>[\s\S]*?(<\/\1>|$)/ig, '' )
-					.replace( /<\/?[a-z][\s\S]*?(>|$)/ig, '' );
-			},
+			return text
+				.replace( /<!--[\s\S]*?(-->|$)/g, '' )
+				.replace( /<(script|style)[^>]*>[\s\S]*?(<\/\1>|$)/ig, '' )
+				.replace( /<\/?[a-z][\s\S]*?(>|$)/ig, '' );
+		},
 
-			/**
-			 * Strip HTML tags and convert HTML entities.
-			 *
-			 * @param {string} text Text.
-			 *
-			 * @return Sanitized text. False on failure.
-			 */
-			sanitizeText: function( text ) {
-				var _text = wp.utils.stripTags( text ),
-					textarea = document.createElement( 'textarea' );
+		/**
+		 * Strip HTML tags and convert HTML entities.
+		 *
+		 * @param {string} text Text.
+		 *
+		 * @return Sanitized text. False on failure.
+		 */
+		sanitizeText: function( text ) {
+			var _text = wp.utils.stripTags( text ),
+				textarea = document.createElement( 'textarea' );
 
-				try {
-					textarea.innerHTML = _text;
-					_text = wp.utils.stripTags( textarea.value );
-				} catch ( er ) {}
+			try {
+				textarea.innerHTML = _text;
+				_text = wp.utils.stripTags( textarea.value );
+			} catch ( er ) {}
 
-				return _text;
-			}
+			return _text;
+		}
 	};
-
 }() );
