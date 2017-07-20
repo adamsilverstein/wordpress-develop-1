92b274f8ee7996753a16a9938d6d789d5f2d774a
Adam Silverstein <adam@10up.com>
2017-07-20 18:22:12 -0400

    babel build

--- src/wp-includes/js/wp-hooks.js
+++ src/wp-includes/js/wp-hooks.js
@@ -224,7 +224,9 @@ function createAddHook(hooks) {
   * @param {Function} callback Function to call when the hook is run
   * @param {?number}  priority Priority of this hook (default=10)
   */
-	return function addHook(hookName, callback, priority) {
+	return function addHook(hookName, callback) {
+		var priority = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 10;
+
 		if (typeof hookName !== 'string') {
 			console.error('The hook name must be a string.');
 			return;
@@ -235,25 +237,17 @@ function createAddHook(hooks) {
 			return;
 		}
 
-		// Assign default priority
-		if ('undefined' === typeof priority) {
-			priority = 10;
-		} else {
-			priority = parseInt(priority, 10);
-		}
-
 		// Validate numeric priority
-		if (isNaN(priority)) {
-			console.error('The hook priority must be omitted or a number.');
+		if (typeof priority !== 'number') {
+			console.error('If specified, the hook priority must be a number.');
 			return;
 		}
 
 		var handler = { callback: callback, priority: priority };
-		var handlers = void 0;
 
 		if (hooks.hasOwnProperty(hookName)) {
 			// Find the correct insert index of the new hook.
-			handlers = hooks[hookName];
+			var handlers = hooks[hookName].handlers;
 			var i = 0;
 			while (i < handlers.length) {
 				if (handlers[i].priority > priority) {
@@ -265,10 +259,11 @@ function createAddHook(hooks) {
 			handlers.splice(i, 0, handler);
 		} else {
 			// This is the first hook of its type.
-			handlers = [handler];
+			hooks[hookName] = {
+				handlers: [handler],
+				runs: 0
+			};
 		}
-
-		hooks[hookName] = handlers;
 	};
 }
 
@@ -310,14 +305,13 @@ function createRemoveHook(hooks, removeAll) {
 		}
 
 		if (removeAll) {
-			var runs = hooks[hookName].runs;
-			hooks[hookName] = [];
-			if (runs) {
-				hooks[hookName].runs = runs;
-			}
+			hooks[hookName] = {
+				runs: hooks[hookName].runs,
+				handlers: []
+			};
 		} else if (callback) {
 			// Try to find the specified callback to remove.
-			var handlers = hooks[hookName];
+			var handlers = hooks[hookName].handlers;
 			for (var i = handlers.length - 1; i >= 0; i--) {
 				if (handlers[i].callback === callback) {
 					handlers.splice(i, 1);
@@ -358,7 +352,7 @@ function createHasHook(hooks) {
   *                            the given hook.
   */
 	return function hasHook(hookName) {
-		return hooks.hasOwnProperty(hookName) ? hooks[hookName].length : 0;
+		return hooks.hasOwnProperty(hookName) ? hooks[hookName].handlers.length : 0;
 	};
 }
 
@@ -399,16 +393,23 @@ function createRunHook(hooks, returnFirstArg) {
 			args[_key - 1] = arguments[_key];
 		}
 
-		var handlers = hooks[hookName];
-		var maybeReturnValue = args[0];
+		if (!hooks.hasOwnProperty(hookName)) {
+			hooks[hookName] = {
+				runs: 0,
+				handlers: []
+			};
+		}
 
-		if (!handlers) {
-			return returnFirstArg ? maybeReturnValue : undefined;
+		var handlers = hooks[hookName].handlers;
+
+		if (!handlers.length) {
+			return returnFirstArg ? args[0] : undefined;
 		}
 
 		hooks.current = hookName;
-		handlers.runs = (handlers.runs || 0) + 1;
+		hooks[hookName].runs++;
 
+		var maybeReturnValue = args[0];
 		handlers.forEach(function (handler) {
 			maybeReturnValue = handler.callback.apply(null, args);
 			if (returnFirstArg) {
