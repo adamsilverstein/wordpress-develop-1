c855b02be34131c0ece3a1b424d8d5f68af391b8
Adam Silverstein <adam@10up.com>
2017-08-02 11:05:55 -0400

    cleanup diff

--- src/wp-includes/js/mce-view.js
+++ src/wp-includes/js/mce-view.js
@@ -502,169 +502,167 @@
 		setIframes: function( head, body, callback, rendered ) {
 			var self = this;
 
-			setTimeout( function() {
-				self.getNodes( function( editor, node ) {
-					var dom = editor.dom,
-						styles = '',
-						bodyClasses = editor.getBody().className || '',
-						editorHead = editor.getDoc().getElementsByTagName( 'head' )[0],
-						iframe, iframeWin, iframeDoc, MutationObserver, observer, i, block;
+			setTimeout( function() { self.getNodes( function( editor, node ) {
+				var dom = editor.dom,
+					styles = '',
+					bodyClasses = editor.getBody().className || '',
+					editorHead = editor.getDoc().getElementsByTagName( 'head' )[0],
+					iframe, iframeWin, iframeDoc, MutationObserver, observer, i, block;
 
-					tinymce.each( dom.$( 'link[rel="stylesheet"]', editorHead ), function( link ) {
-						if ( link.href && link.href.indexOf( 'skins/lightgray/content.min.css' ) === -1 &&
-							link.href.indexOf( 'skins/wordpress/wp-content.css' ) === -1 ) {
+				tinymce.each( dom.$( 'link[rel="stylesheet"]', editorHead ), function( link ) {
+					if ( link.href && link.href.indexOf( 'skins/lightgray/content.min.css' ) === -1 &&
+						link.href.indexOf( 'skins/wordpress/wp-content.css' ) === -1 ) {
 
-							styles += dom.getOuterHTML( link );
-						}
-					} );
-
-					if ( self.iframeHeight ) {
-						dom.add( node, 'span', {
-							'data-mce-bogus': 1,
-							style: {
-								display: 'block',
-								width: '100%',
-								height: self.iframeHeight
-							}
-						}, '\u200B' );
+						styles += dom.getOuterHTML( link );
 					}
+				} );
 
-					editor.undoManager.transact( function() {
-						node.innerHTML = '';
-
-						iframe = dom.add( node, 'iframe', {
-							/* jshint scripturl: true */
-							src: tinymce.Env.ie ? 'javascript:""' : '',
-							frameBorder: '0',
-							allowTransparency: 'true',
-							scrolling: 'no',
-							'class': 'wpview-sandbox',
-							style: {
-								width: '100%',
-								display: 'block'
-							},
+				if ( self.iframeHeight ) {
+					dom.add( node, 'span', {
+						'data-mce-bogus': 1,
+						style: {
+							display: 'block',
+							width: '100%',
 							height: self.iframeHeight
-						} );
+						}
+					}, '\u200B' );
+				}
 
-						dom.add( node, 'span', { 'class': 'mce-shim' } );
-						dom.add( node, 'span', { 'class': 'wpview-end' } );
+				editor.undoManager.transact( function() {
+					node.innerHTML = '';
+
+					iframe = dom.add( node, 'iframe', {
+						/* jshint scripturl: true */
+						src: tinymce.Env.ie ? 'javascript:""' : '',
+						frameBorder: '0',
+						allowTransparency: 'true',
+						scrolling: 'no',
+						'class': 'wpview-sandbox',
+						style: {
+							width: '100%',
+							display: 'block'
+						},
+						height: self.iframeHeight
 					} );
 
-					// Bail if the iframe node is not attached to the DOM.
-					// Happens when the view is dragged in the editor.
-					// There is a browser restriction when iframes are moved in the DOM. They get emptied.
-					// The iframe will be rerendered after dropping the view node at the new location.
-					if ( ! iframe.contentWindow ) {
+					dom.add( node, 'span', { 'class': 'mce-shim' } );
+					dom.add( node, 'span', { 'class': 'wpview-end' } );
+				} );
+
+				// Bail if the iframe node is not attached to the DOM.
+				// Happens when the view is dragged in the editor.
+				// There is a browser restriction when iframes are moved in the DOM. They get emptied.
+				// The iframe will be rerendered after dropping the view node at the new location.
+				if ( ! iframe.contentWindow ) {
+					return;
+				}
+
+				iframeWin = iframe.contentWindow;
+				iframeDoc = iframeWin.document;
+				iframeDoc.open();
+
+				iframeDoc.write(
+					'<!DOCTYPE html>' +
+					'<html>' +
+						'<head>' +
+							'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+							head +
+							styles +
+							'<style>' +
+								'html {' +
+									'background: transparent;' +
+									'padding: 0;' +
+									'margin: 0;' +
+								'}' +
+								'body#wpview-iframe-sandbox {' +
+									'background: transparent;' +
+									'padding: 1px 0 !important;' +
+									'margin: -1px 0 0 !important;' +
+								'}' +
+								'body#wpview-iframe-sandbox:before,' +
+								'body#wpview-iframe-sandbox:after {' +
+									'display: none;' +
+									'content: "";' +
+								'}' +
+								'iframe {' +
+									'max-width: 100%;' +
+								'}' +
+							'</style>' +
+						'</head>' +
+						'<body id="wpview-iframe-sandbox" class="' + bodyClasses + '">' +
+							body +
+						'</body>' +
+					'</html>'
+				);
+
+				iframeDoc.close();
+
+				function resize() {
+					var $iframe;
+
+					if ( block ) {
 						return;
 					}
 
-					iframeWin = iframe.contentWindow;
-					iframeDoc = iframeWin.document;
-					iframeDoc.open();
+					// Make sure the iframe still exists.
+					if ( iframe.contentWindow ) {
+						$iframe = $( iframe );
+						self.iframeHeight = $( iframeDoc.body ).height();
 
-					iframeDoc.write(
-						'<!DOCTYPE html>' +
-						'<html>' +
-							'<head>' +
-								'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
-								head +
-								styles +
-								'<style>' +
-									'html {' +
-										'background: transparent;' +
-										'padding: 0;' +
-										'margin: 0;' +
-									'}' +
-									'body#wpview-iframe-sandbox {' +
-										'background: transparent;' +
-										'padding: 1px 0 !important;' +
-										'margin: -1px 0 0 !important;' +
-									'}' +
-									'body#wpview-iframe-sandbox:before,' +
-									'body#wpview-iframe-sandbox:after {' +
-										'display: none;' +
-										'content: "";' +
-									'}' +
-									'iframe {' +
-										'max-width: 100%;' +
-									'}' +
-								'</style>' +
-							'</head>' +
-							'<body id="wpview-iframe-sandbox" class="' + bodyClasses + '">' +
-								body +
-							'</body>' +
-						'</html>'
-					);
-
-					iframeDoc.close();
-
-					function resize() {
-						var $iframe;
-
-						if ( block ) {
-							return;
-						}
-
-						// Make sure the iframe still exists.
-						if ( iframe.contentWindow ) {
-							$iframe = $( iframe );
-							self.iframeHeight = $( iframeDoc.body ).height();
-
-							if ( $iframe.height() !== self.iframeHeight ) {
-								$iframe.height( self.iframeHeight );
-								editor.nodeChanged();
-							}
+						if ( $iframe.height() !== self.iframeHeight ) {
+							$iframe.height( self.iframeHeight );
+							editor.nodeChanged();
 						}
 					}
+				}
 
-					if ( self.iframeHeight ) {
-						block = true;
+				if ( self.iframeHeight ) {
+					block = true;
+
+					setTimeout( function() {
+						block = false;
+						resize();
+					}, 3000 );
+				}
+
+				function reload() {
+					if ( ! editor.isHidden() ) {
+						$( node ).data( 'rendered', null );
 
 						setTimeout( function() {
-							block = false;
-							resize();
-						}, 3000 );
-					}
-
-					function reload() {
-						if ( ! editor.isHidden() ) {
-							$( node ).data( 'rendered', null );
-
-							setTimeout( function() {
-								wp.mce.views.render();
-							} );
-						}
-					}
-
-					function addObserver() {
-						observer = new MutationObserver( _.debounce( resize, 100 ) );
-
-						observer.observe( iframeDoc.body, {
-							attributes: true,
-							childList: true,
-							subtree: true
+							wp.mce.views.render();
 						} );
 					}
+				}
 
-					$( iframeWin ).on( 'load', resize ).on( 'unload', reload );
+				function addObserver() {
+					observer = new MutationObserver( _.debounce( resize, 100 ) );
 
-					MutationObserver = iframeWin.MutationObserver || iframeWin.WebKitMutationObserver || iframeWin.MozMutationObserver;
+					observer.observe( iframeDoc.body, {
+						attributes: true,
+						childList: true,
+						subtree: true
+					} );
+				}
 
-					if ( MutationObserver ) {
-						if ( ! iframeDoc.body ) {
-							iframeDoc.addEventListener( 'DOMContentLoaded', addObserver, false );
-						} else {
-							addObserver();
-						}
+				$( iframeWin ).on( 'load', resize ).on( 'unload', reload );
+
+				MutationObserver = iframeWin.MutationObserver || iframeWin.WebKitMutationObserver || iframeWin.MozMutationObserver;
+
+				if ( MutationObserver ) {
+					if ( ! iframeDoc.body ) {
+						iframeDoc.addEventListener( 'DOMContentLoaded', addObserver, false );
 					} else {
-						for ( i = 1; i < 6; i++ ) {
-							setTimeout( resize, i * 700 );
-						}
+						addObserver();
 					}
+				} else {
+					for ( i = 1; i < 6; i++ ) {
+						setTimeout( resize, i * 700 );
+					}
+				}
 
-					callback && callback.call( self, editor, node );
-				}, rendered );
-			}, 0 );
+				callback && callback.call( self, editor, node );
+			}, rendered ); }, 0 );
 		},
 
 		/**
