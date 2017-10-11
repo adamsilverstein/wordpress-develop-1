35bc2353d2b7b2d7d1534419008801cba2f1e19b
Adam Silverstein <adam@10up.com>
2017-10-11 11:50:48 -0400

    update built file

--- src/wp-includes/js/media-grid.js
+++ src/wp-includes/js/media-grid.js
@@ -795,7 +795,7 @@ EditAttachments = MediaFrame.extend(/** @lends wp.media.view.MediaFrame.EditAtta
 	 */
 	previousMediaItem: function() {
 		if ( ! this.hasPrevious() ) {
-			this.$( '.left' ).blur();
+			this.$el.focus();
 			return;
 		}
 		this.trigger( 'refresh', this.library.at( this.getCurrentIndex() - 1 ) );
@@ -807,7 +807,7 @@ EditAttachments = MediaFrame.extend(/** @lends wp.media.view.MediaFrame.EditAtta
 	 */
 	nextMediaItem: function() {
 		if ( ! this.hasNext() ) {
-			this.$( '.right' ).blur();
+			this.$el.focus();
 			return;
 		}
 		this.trigger( 'refresh', this.library.at( this.getCurrentIndex() + 1 ) );
