af491b40b6a50b4a2d70f5c05711f1dd25cf4abc
Adam Silverstein <adam@10up.com>
2018-02-16 20:16:14 -0500

    trigger keyup when clearing search

--- src/wp-admin/js/customize-nav-menus.js
+++ src/wp-admin/js/customize-nav-menus.js
@@ -727,20 +727,8 @@
 			$( 'body' ).removeClass( 'adding-menu-items' );
 			$( '#available-menu-items .menu-item-handle.item-added' ).removeClass( 'item-added' );
 
-			this.$search.val( '' );
+			this.$search.val( '' ).trigger( 'keyup' );;
 
-			// Clear search fields.
-			if ( this.$clearResults.hasClass( 'open' ) ) {
-				this.$clearResults.removeClass( 'open' );
-			}
-			
-			if ( this.$clearResults.hasClass( 'is-visible' ) ) {
-				this.$clearResults.removeClass( 'is-visible' );
-			}
-
-			if ( this.sectionContent.is(':visible') ) {
-				this.sectionContent.hide();
-			}
 		},
 
 		// Add a few keyboard enhancements to the panel.
