185b95bbc231c1467ac524350f13c3e6743b431a
Adam Silverstein <adam@10up.com>
2019-07-30 14:24:01 -0600

    docblock cleanup

--- src/wp-includes/class-wp-comment-query.php
+++ src/wp-includes/class-wp-comment-query.php
@@ -382,16 +382,15 @@ class WP_Comment_Query {
 		$comment_ids = null;
 
 		/**
-		 * Filter the comments array before the query takes place.
+		 * Filter the comments data before the query takes place.
 		 *
 		 * Return a non-null value to bypass WordPress's default comment queries.
 		 *
-		 *
 		 * @since 5.3.0
 		 *
-		 * @param array|null    $comment_ids Return an array of comment data to short-circuit WP's comment query,
-		 *                                or null to allow WP to run its normal queries.
-		 * @param WP_Comment_Query $this The WP_Comment_Query instance, passed by reference.
+		 * @param array|null       $comment_ids Return an array of comment data to short-circuit WP's comment query,
+		 *                                      or null to allow WP to run its normal queries.
+		 * @param WP_Comment_Query $this        The WP_Comment_Query instance, passed by reference.
 		 */
 		$comment_ids = apply_filters_ref_array( 'comments_pre_query', array( $comment_ids, &$this ) );
 
